23b6849eca9152f60b6779ecb72dc725d037d861
Elanur Bulbul <126582672+elanurbulbul@users.noreply.github.com>
2024-02-17 13:07:04 +0300

    CommenysLoading

--- src/pages/Comment/CommentDetail.js
+++ src/pages/Comment/CommentDetail.js
@@ -1,17 +1,22 @@
 import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router'
+import Loading from '../../components/Loading';
 
 export default function CommentDetail() {
   const { id } = useParams();
 
   const [commentItem, setCommentItem] = useState([]);
+  const [loading, setLoading] = useState(false);
+
 
   useEffect(() => {
+    setLoading(true);
     fetch(` https://jsonplaceholder.typicode.com/comments/${id}`)
       .then((res) => {
         return res.json();
       })
       .then((data) => {
+        setLoading(false);
         console.log(data);
         setCommentItem(data);
       });
@@ -19,6 +24,7 @@ export default function CommentDetail() {
 
   return (
     <div>
+      {loading && <Loading />}
       ID:{id} 
       <h1>{commentItem.name}</h1>
       <p>{commentItem.email}</p>
--- src/pages/Comment/index.js
+++ src/pages/Comment/index.js
@@ -2,17 +2,21 @@ import React from 'react'
 import CommentCard from '../../components/Comments';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Loading from '../../components/Loading';
 
 
 export default function Comments() {
   const [comment, setComment] = useState([]);
+  const [loading, setLoading]= useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch('https://jsonplaceholder.typicode.com/comments')
       .then((res) => {
         return res.json();
       })
       .then((data) => {
+        setLoading(false);
         console.log(data);
         setComment(data);
       });
@@ -21,6 +25,7 @@ export default function Comments() {
 
   return (
     <div>
+      {loading && <Loading />}
       <div class="grid-container">
         {comment.map((commentItem) => (
           <div class="grid-item">
